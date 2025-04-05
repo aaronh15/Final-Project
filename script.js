22e9539ab985c0306dae7942c977e00e76a9c7d1
aaronh15 <the.aaron.huang@gmail.com>
2025-04-05 21:49:20 +0000

    final

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var header = document.getElementById("main-title");
 
+header.style.textAlign = "center";
+
+var description = document.getElementById("pinyin");
+
+description.style.textAlign = "center";
+
+var characterList = document.getElementById("character-list");
+
+characterList.style.justifyContent = "space-between";
+
+var characterImages = document.getElementsByClassName("character-img");
